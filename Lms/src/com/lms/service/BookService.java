b005305e59c1588bb72cfa42042809616847da3a
Tulasidhar <110711019+TulasidharM@users.noreply.github.com>
2025-07-24 12:34:33 +0530

    Added Basic interfaces and structure

---
+++ Lms/src/com/lms/service/BookService.java
@@ -0,0 +1,12 @@
+package com.lms.service;
+
+import java.util.List;
+
+import com.lms.model.Book;
+
+public interface BookService {
+	
+	public void addNewBook(Book newBook);
+	public List<Book> getAllBooks();
+	public void updateBookAvailability(Book book);
+}
---
+++ Lms/src/com/lms/service/impl/BookServiceImpl.java
@@ -0,0 +1,22 @@
+package com.lms.service.impl;
+
+import java.util.List;
+
+import com.lms.model.Book;
+import com.lms.service.BookService;
+
+public class BookServiceImpl implements BookService{
+	@Override
+	public void addNewBook(Book newBook) {
+		// TODO Auto-generated method stub
+		
+	}@Override
+	public List<Book> getAllBooks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void updateBookAvailability(Book book) {
+		// TODO Auto-generated method stub
+	}
+}
